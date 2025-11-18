7e5ae8ef96a77261b8c2b08cb30cae97d1e388d2
TimurT <ofset989@gmai.com>
2025-11-18 18:35:15 +0100

    Second commit

--- lesson8/myapp.py
+++ lesson8/myapp.py
@@ -16,28 +16,27 @@ class Point():
     def falls_in_rectangle(self, point1_coordinate, point2_coordinate):
         self.point1_coordinate  = point1_coordinate
         self.point2_coordinate  = point2_coordinate
-        if self.point1_coordinate.x <= self.x <= self.point2_coordinate.x and self.point1_coordinate.y <= self.x <= self.point2_coordinate.y:
+        if self.point1_coordinate.x <= self.x <= self.point2_coordinate.x and self.point1_coordinate.y <= self.y <= self.point2_coordinate.y:
             global win_counter_global
             win_counter_global +=1
-            print("The point falls in rectangle")
-            print(f"Your win counter is {win_counter_global}")
-            print (point1_coordinate,point2_coordinate)
-            
+            return True
             
 
-        elif self.point1_coordinate.x >= self.x >= self.point2_coordinate.x and self.point1_coordinate.y >= self.y >= self.point2_coordinate:
+        elif self.point1_coordinate.x >= self.x >= self.point2_coordinate.x and self.point1_coordinate.y >= self.y >= self.point2_coordinate.y:
             win_counter_global +=1
-            print("The point falls in rectangle")
-            print(f"Your win counter is {win_counter_global}")
-            print (point1_coordinate,point2_coordinate)
+            return True
+            # print("The point falls in rectangle")
+            # print(f"Your win counter is {win_counter_global}")
+            # print (point1_coordinate,point2_coordinate)
              
          
         else:
             global fall_counter_global
             fall_counter_global +=1
-            print("The point doesnt fall in rectangle")
-            print(f"Your fall counter is {fall_counter_global}")
-            print (point1_coordinate,point2_coordinate)
+            return False
+            # print("The point doesnt fall in rectangle")
+            # print(f"Your fall counter is {fall_counter_global}")
+            # print (point1_coordinate,point2_coordinate)
             
 
 termination = "No"
@@ -50,6 +49,12 @@ while termination != "Yes":
     y= int(input("y="))
     user_point = Point (x,y)
     user_point.falls_in_rectangle(point1, point2) 
+    if user_point.falls_in_rectangle == True:
+        print(f"""
+                The point falls in rectangle
+                Your win counter is {win_counter_global}
+                {point1},{point2}
+     """)
     termination = input("Do you want to end? Please use 'Yes' or 'No'")
 
 if termination == "Yes":
